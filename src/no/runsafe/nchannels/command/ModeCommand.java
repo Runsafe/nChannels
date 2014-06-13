df1c14fade9284baab48d8b013d1e1a359af2d49
Morten Nilsen <morten@runsafe.no>
2014-06-14 00:02:52 +0200

    Use new argument type

--- src/no/runsafe/nchannels/command/ModeCommand.java
+++ src/no/runsafe/nchannels/command/ModeCommand.java
@@ -1,9 +1,9 @@
 package no.runsafe.nchannels.command;
 
 import no.runsafe.framework.api.IScheduler;
+import no.runsafe.framework.api.command.argument.BooleanArgument;
 import no.runsafe.framework.api.command.argument.Enumeration;
 import no.runsafe.framework.api.command.argument.IArgumentList;
-import no.runsafe.framework.api.command.argument.RequiredArgument;
 import no.runsafe.framework.api.command.player.PlayerAsyncCommand;
 import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.nchannels.model.Mode;
@@ -12,7 +12,7 @@ public class ModeCommand extends PlayerAsyncCommand
 {
 	public ModeCommand(IScheduler scheduler)
 	{
-		super("mode", "Change channel mode flags", "runsafe.channel.<channel>.mode", scheduler, new Enumeration("mode", Mode.values()).require(), new RequiredArgument("on"));
+		super("mode", "Change channel mode flags", "runsafe.channel.<channel>.mode", scheduler, new Enumeration("mode", Mode.values()).require(), new BooleanArgument("on").require());
 	}
 
 	@Override
