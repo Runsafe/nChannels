ac1f728f3a9c584ddf99ec0e01d567e9bfd59774
Morten Nilsen <morten@runsafe.no>
2014-06-13 20:08:30 +0200

    Initialized new plugin nChannels from template

---
+++ src/no/runsafe/nchannels/nChannels.java
@@ -0,0 +1,26 @@
+package no.runsafe.nchannels;
+
+import no.runsafe.framework.RunsafePlugin;
+
+public class nChannels extends RunsafePlugin
+//public class nChannels extends RunsafeConfigurablePlugin
+{
+//	public static IDebug Debugger = null;
+
+	@Override
+	protected void pluginSetup()
+	{
+//		Debugger = getComponent(IDebug.class);
+
+		// Framework features
+//		addComponent(Commands.class);
+//		addComponent(Database.class);
+//		addComponent(Events.class);
+//		addComponent(FrameworkHooks.class);
+//		addComponent(LUAScripts.class);
+//		addComponent(UniverseRegistration.class);
+
+		// Plugin components
+//		addComponent(SomeComponent.class);
+	}
+}
